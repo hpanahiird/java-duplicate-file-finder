5cbacf3b58be4eaceee51b943bccd9482eafd84d
h.panahi.ird <h.panahi.ird@gmail.com>
2019-05-18 02:00:35 +0430

    determine input file type.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
 
-    String directory;
+    private String directory;
 
     public static void main(String[] args) {
         System.out.println("Hello");
         (new Main()).getInputs();
     }
 
-    void getInputs(){
+    private void getInputs(){
         System.out.println("Please enter your directory: ");
         Scanner scanner = new Scanner(System.in);
         this.directory = scanner.nextLine();
+        File root = new File(directory);
+        if (root.isFile())
+            System.out.println("is file");
+        else if (root.isDirectory())
+            System.out.println("is dir");
+        else
+            System.out.println("nothing");
     }
 }
