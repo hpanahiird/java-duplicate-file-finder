83a421815488620a1b9dec09e4997dc86e71de8b
h.panahi.ird <h.panahi.ird@gmail.com>
2019-05-18 01:53:12 +0430

    get directory to be scanned for duplicates.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
+    String directory;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello");
+        (new Main()).getInputs();
+    }
+
+    void getInputs(){
+        System.out.println("Please enter your directory: ");
+        Scanner scanner = new Scanner(System.in);
+        this.directory = scanner.nextLine();
     }
 }
