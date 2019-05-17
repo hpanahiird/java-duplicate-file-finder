580e17e3d0cc6abb5152ccbc505c59b4da2c9c68
h.panahi.ird <h.panahi.ird@gmail.com>
2019-05-18 02:36:30 +0430

    put sub-directories to a queue to be scanned and put files to a hashmap with a key equal to file size.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
     private String directory;
+    private Queue<File> directories = new LinkedList<>();
+    private HashMap<String, String> files = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Hello");
@@ -24,6 +29,25 @@ public class Main {
             else
                 System.out.println("\033[0;31m" + "nothing" + "\033[0m");
         } while (!root.isDirectory());
+        directories.add(root);
+        while (directories.size() != 0) {
+            File currentFile = directories.poll();
+            for (String child :
+                    currentFile.list()) {
+                System.out.println(child);
+                File file = new File(currentFile, child);
+                if (file.isDirectory())
+                    directories.add(file);
+                else if (file.isFile()) {
+//                    if (files.containsKey(file.length()+"")) {
+//                        System.out.println(files.get(file.length()+""));
+//                        System.out.println(file.getAbsolutePath());
+//                    }
+                    files.put(file.length() + "", file.getAbsolutePath());
+                }
 
+            }
+        }
+        System.out.println(files.size());
     }
 }
